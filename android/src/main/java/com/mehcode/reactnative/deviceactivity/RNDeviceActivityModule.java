a79321d2b5fabfbdb84fb5f01b764f367695b92a
Ryan Leckey <ryan@launchbadge.com>
2016-07-30 02:57:15 -0700

    :bug:

--- android/src/main/java/com/mehcode/reactnative/deviceactivity/RNDeviceActivityModule.java
+++ android/src/main/java/com/mehcode/reactnative/deviceactivity/RNDeviceActivityModule.java
@@ -6,8 +6,10 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings.Secure;
 
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -28,9 +30,9 @@ public class RNDeviceActivityModule extends ReactContextBaseJavaModule implement
     }
 
     private void emit(String eventName) {
-      getReactApplicationContext().
-          getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).
-          emit("RNDeviceActivity:" + eventName, null);
+        getReactApplicationContext().
+                getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).
+                emit("RNDeviceActivity:" + eventName, null);
     }
 
     @Override
